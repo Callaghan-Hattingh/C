5d330c76a77ce76e07948d2b53d73c46b61e50b5
callaghan <hattinghcallaghan@gmail.com>
2022-04-01 07:37:51 +0200

    math ops

--- C Programming for Beginners/4_arithmetic.c
+++ C Programming for Beginners/4_arithmetic.c
@@ -3,16 +3,22 @@
 int main(void)
 {
     double x = 5.24, y = -1.78;
+    int a = 11, b = 3;
 
     double mult = x * y;
     double div = x / y;
     double add = x + y;
     double sub = x - y;
+    int mod = a % b;
+    int intdiv = a / b;
+    
 
     printf("mult: %f\n", mult);
     printf("div: %f\n", div);
     printf("add: %f\n", add);
     printf("sub: %f\n", sub);
+    printf("mod: %d\n", mod);
+    printf("intdiv: %d\n", intdiv);
 
     return 0;
 }
